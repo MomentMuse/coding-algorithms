47a635a7cf1a9ce214b2b3aca1fae1a2018824e5
Amy Kirasack <amykirasack@gmail.com>
2018-05-29 23:39:43 -0400

    Practice palindrom algorithms

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,33 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// My Solutions: Hack method way, simple for loop
+// Remember: Your solution should return a boolean.
+
+// function palindrome(str) {
+//     const strRev = str.split('').reverse().join('');
+//     return str === strRev;
+// }
+
+// function palindrome(str) {
+//     const strRev = '';
+//     for (let i = str.length -1; i >= 0; i--) {
+//         strRev += str[i];
+//     }
+//     return str === strRev;
+// }
+
+// Alternate Solution with every array helper
+// Technically not idea, because you are doing twice as much work than necessary
+// It's a good idea to show your interviewer that you can solve problems in multiple ways! And compare and contrast them
+
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+// arrays are 0 indexed, so you need to add the -1
+// compares the first char to last char, second char to second last char, and so on
+        return char === str[str.length - i - 1];
+    });
+}
+
 
 module.exports = palindrome;
