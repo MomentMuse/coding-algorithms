d376e2588af2de37abaf6805f45b0bb30a18093c
Amy Kirasack <amykirasack@gmail.com>
2018-05-30 01:16:06 -0400

    Practice reverseint algorithm

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// turn number into a string, then split into an array
+// apply reverse and then join again to form a string
+// parse reversed string into an integer (this removes the sign and extra zeroes)
+// multiply this value by Math.sign(n) (negative n returns -1, positive n returns 1)
+
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
