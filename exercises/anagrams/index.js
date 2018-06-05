c09f4098e070bf24e8d9ce4031cf2f54eb2fcf3e
Amy Kirasack <amykirasack@gmail.com>
2018-06-04 22:48:23 -0400

    Practice anagram algorithms with character map, Object.key, and split('').sort().join()

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,56 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// RegExp regular expressions \w disregard spaces, punctuation
+// const word = "HI, THERE!!!?"
+// word.replace(/[^\w]/g, "").toLowerCase() returns "hithere"
+// build a character map to compare strings (objects with key-value pairs)
+
+function anagrams(stringA, stringB) {
+//use helper function to build charMap objects for str A and B
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+//pulls keys of object and returns array; tells you numbers of keys in an object
+//if number of keys in a !== number of keys in b, return false
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+//if they do have the same number of characters, use for in loop to iterate over keys in object
+//compare values of keys, if same, return true
+    for(let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+//remember to return your result!
+    return true;
+}
+
+//build helper function so you don't need to write the same loop for both strings
+function buildCharMap(str) {
+//create empty object for character map
+    const charMap = {};
+//for of loop iterating over string with and replace all characters that are not alphanumeric with an empty string (thus deleting all punctuation and spaces)
+//all characters to lower case
+//regex are hard; study them more!!
+    for(let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//OR statement that adds one to charMap[chap] if key value pair exits; if not, creates key value pair and assigns value =1
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+//remember to RETURN the charMap object to the function call!!
+    return charMap;
+}
+
+//My solution using split, sort, join
+//It's the same as the alternative solution! (except I originally split it into cleanString, sortString helper func)
+//I abstracted the functions away so you don't have to call twice each manually
+//Compares sorted strings with punctuation removed
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join()
+}
 
 module.exports = anagrams;
