ea2ad95c72ce492c640a31682b54e8308a431fb5
Amy Kirasack <amykirasack@gmail.com>
2018-06-29 18:38:59 -0400

    Practice regex for vowels and .match

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,30 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//My attempt
+// function vowels(str) {
+//     return str.match(/[aeiou]/gi).length;
+// }
+//fails on an edge case when there are no vowels
+
+//Solution #1: Iterative
+function vowels(str) {
+    let vowelCount = 0;
+    const vowelCheck = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()) {
+        if (vowelCheck.includes(char)) {
+            vowelCount++;
+        }
+    }
+    return vowelCount;
+}
+
+//Solution #2
+function vowels(str) {
+   const matches = str.match(/[aeiou]/gi);
+   // if no matches are found, matches will return null
+   return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
